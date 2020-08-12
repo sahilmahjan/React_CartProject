51fb6036b7ba6b0f2cc44b2aee44f70a21f76618
Sahil <sahil.mahajan9937@gmail.com>
2020-08-12 14:10:14 +0530

    updated version

--- src/CartItem.js
+++ src/CartItem.js
@@ -11,11 +11,75 @@ class CartItem extends React.Component {
         }
     // or
         // this.increseQuantity=this.increseQuantity.bind(this);
-        }
+        } 
+
+
+
+
+
     // applied
     increseQuantity = () => {
-        console.log('this.state', this.state);
+        // this.state.qty+=1;
+        // console.log('this.state', this.state);
+
+        // set state form 1
+        // object form
+        // this.setState({
+        //     // shallow merging
+        //     qty: this.state.qty +1
+        // });
+
+        // increase by 3
+        // this.setState({
+        //     // shallow merging
+        //     qty: this.state.qty +3
+        // });
+
+
+        // set state form 2- if prevState required use this
+        // function form
+        this.setState((prevState) =>{
+            return{
+                qty: prevState.qty+1
+            }
+            
+        },
+        // for console purpose
+        //  () =>
+        // {
+        //     console.log('this.state', this.state);
+        // }
+        );
+        
     }
+
+    decreseQuantity = () => {
+        const {qty} = this.state;
+        if(qty==0)
+        return;
+        // this.state.qty+=1;
+        // console.log('this.state', this.state);
+
+        // set state form 1
+        // object form
+        // this.setState({
+        //     // shallow merging
+        //     qty: this.state.qty +1
+        // });
+
+
+        // set state form 2- if prevState required use this
+        // function form
+        this.setState((prevState) =>{
+            return{
+                qty: prevState.qty-1
+            }
+            
+        });
+    }
+
+
+
     render(){
         const { price,title, qty}=this.state;
         return(
@@ -45,7 +109,9 @@ class CartItem extends React.Component {
                             onClick={this.increseQuantity}
                             /> 
 
-                            <img alt="decrease"  className="action-icons" src="https://image.flaticon.com/icons/svg/992/992683.svg" />
+                            <img alt="decrease"  className="action-icons" src="https://image.flaticon.com/icons/svg/992/992683.svg" 
+                            onClick={this.decreseQuantity}
+                            />
 
                             <img alt="delete"  
                             className="action-icons" src="https://image.flaticon.com/icons/svg/1345/1345874.svg" />
